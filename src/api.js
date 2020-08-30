0e6a3a0c179e139247ff7ce303da051914045e52
Yaroslav Ryzhkov <666anastyalena@mail.ru>
2020-08-30 21:06:28 +0300

    feat: start HTML layout

--- src/api.js
+++ src/api.js
@@ -1,19 +1,17 @@
-const IMAGE_ACCES_KEY = 'Nw_vIJ8V12FenylBYs53wAic7GMfJAEhjdcgQAsaiLo';
-let image_counter = 0;
-let query_word = 'Belarus';
-// const IMAGE_SECRET_KEY = 'SVhVqGmWAEsDZERUOUAuShQdWqylfOLhKbwyo73nsH8';
+// const IMAGE_ACCES_KEY = 'Nw_vIJ8V12FenylBYs53wAic7GMfJAEhjdcgQAsaiLo';
+// let image_counter = 0;
+// let query_word = 'Belarus';
+// // const IMAGE_SECRET_KEY = 'SVhVqGmWAEsDZERUOUAuShQdWqylfOLhKbwyo73nsH8';
 
-function getSearchUrl(searchQuery = "Minsk") {
-    return `https://api.unsplash.com/search/photos?query=${searchQuery}&client_id=${IMAGE_ACCES_KEY}`;
-}
+// function getSearchUrl(searchQuery = "Minsk") {
+//     return `https://api.unsplash.com/search/photos?query=${searchQuery}&client_id=${IMAGE_ACCES_KEY}`;
+// }
 
 
-fetch(getSearchUrl(query_word))
-    .then(res => res.json())
-    .then(res => {
-        document.querySelector('body').background = `${res.results[image_counter].urls.full}`;
-        return res;
-    })
-    .catch(e => new Error(e))
-
-console.log()
+// fetch(getSearchUrl(query_word))
+//     .then(res => res.json())
+//     .then(res => {
+//         document.querySelector('body').background = `${res.results[image_counter].urls.full}`;
+//         return res;
+//     })
+//     .catch(e => new Error(e))
